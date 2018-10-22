7b1a6ba872fab19d88060c5ee0a8499febd1147b
oliver bannister <oliver.bannister@gamesys.co.uk>
2018-10-22 16:27:22 +0100

    added a bigger cube rotating around the smaller cube

--- rotatingCube/app.js
+++ rotatingCube/app.js
@@ -7,7 +7,7 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 controls = new THREE.OrbitControls(camera, renderer.domElement);
 
-var cubeMaterials = [
+var smallCubeMaterials = [
     new THREE.MeshLambertMaterial({ map: new THREE.TextureLoader().load('images/1_dot.png'), side: THREE.DoubleSide }),
     new THREE.MeshLambertMaterial({ map: new THREE.TextureLoader().load('images/2_dots.png'), side: THREE.DoubleSide }),
     new THREE.MeshLambertMaterial({ map: new THREE.TextureLoader().load('images/3_dots.png'), side: THREE.DoubleSide }),
@@ -16,31 +16,29 @@ var cubeMaterials = [
     new THREE.MeshLambertMaterial({ map: new THREE.TextureLoader().load('images/6_dots.png'), side: THREE.DoubleSide })
 ];
 var geometry = new THREE.BoxGeometry(1, 1, 1);
-var material = new THREE.MeshFaceMaterial(cubeMaterials);
+var material = new THREE.MeshFaceMaterial(smallCubeMaterials);
 var cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
 
-
-
-
-var floorGeometry = new THREE.CubeGeometry(5, 1, 5);
+var floorGeometry = new THREE.CubeGeometry(5, 5, 5);
 var floorMaterial = new THREE.MeshBasicMaterial({ color: '#433F81', side: THREE.DoubleSide, wireframe: true });
-var floorCube = new THREE.Mesh(floorGeometry, floorMaterial);
-floorCube.position.y = -2;
-scene.add(floorCube);
+var bigCube = new THREE.Mesh(floorGeometry, floorMaterial);
+bigCube.position.y = -0.25;
+scene.add(bigCube);
 
-
-
-camera.position.z = 5;
+camera.position.z = 7;
 
 var ambientLight = new THREE.AmbientLight(0xFFFFFF, 3.0);
 scene.add(ambientLight);
 
 var render = function () {
     requestAnimationFrame(render);
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    cube.rotation.x += Math.random() / 70;
+    cube.rotation.y += Math.random() / 70;
+    bigCube.rotation.x -= Math.random() / 70;
+    bigCube.rotation.y -= Math.random() / 70;
+
     renderer.render(scene, camera);
 };
 
